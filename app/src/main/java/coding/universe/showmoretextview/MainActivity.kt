3f516aac59fc631572dc84888ddee2b84c7696f8
aayush287 <aayushchaudhary287@gmail.com>
2021-01-23 15:47:14 +0530

    type: Initial commit

---
+++ app/src/main/java/coding/universe/showmoretextview/MainActivity.kt
@@ -0,0 +1,11 @@
+package coding.universe.showmoretextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ showmoretextview/src/main/java/coding/universe/showmoretextview/ShowMoreTextView.kt
@@ -0,0 +1,11 @@
+package coding.universe.showmoretextview
+
+
+import android.content.Context
+import android.util.AttributeSet
+
+
+
+class ShowMoreTextView(context: Context, attrs: AttributeSet?) : androidx.appcompat.widget.AppCompatTextView(context, attrs) {
+
+}
