26c48a795d6b7f80d1a0477bf8d68f3182a9e25b
aayush287 <aayushchaudhary287@gmail.com>
2021-04-05 18:49:04 +0530

    type : ClickableSpan Added

--- showmoretextview/src/main/java/coding/universe/showmoretextview/ShowMoreTextView.kt
+++ showmoretextview/src/main/java/coding/universe/showmoretextview/ShowMoreTextView.kt
@@ -3,15 +3,15 @@ package coding.universe.showmoretextview
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.graphics.Color
-import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import android.widget.TextView
-import kotlin.math.E
 
 
 class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
@@ -23,14 +23,16 @@ class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
     }
 
     private var textString: CharSequence? = null
-    private val readMore = true
     private var endCharacterIndex = 0
     private var maxLinesVisible = DEFAULT_MAX_LINE
     private var collapsedText: CharSequence? = null
     private var expandedText: CharSequence? = null
     private var lastIndexOfText = 0
     private val colorClickableText = 0
-    private val showTrimExpandedText = false
+    private var showFullExpandedText = false
+    private val viewMoreSpan by lazy { ShowMoreClickableSpan() }
+
+    private var readMore = true
 
     private var bufferType: BufferType? = null
 
@@ -44,35 +46,31 @@ class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
 
     private fun setText() {
         super.setText(getDisplayableText())
-        invalidate()
-        requestLayout()
+        movementMethod = LinkMovementMethod.getInstance();
+        highlightColor = Color.TRANSPARENT;
     }
 
     private fun getDisplayableText(): CharSequence? {
-        return getTrimmedText(text)
+        return getTrimmedText(textString)
     }
 
     private fun getTrimmedText(text: CharSequence?): CharSequence? {
-        return getCollapsedText(text)
+        return if (readMore) {
+            getCollapsedText(text)
+        } else {
+            getExpandedText(text)
+        }
     }
 
-    private fun getCollapsedText(text: CharSequence?): CharSequence? {
-        Log.d(TAG, "getCollapsedText: $ELLIPSIZE")
-        Log.d(TAG, "getCollapsedText: $collapsedText")
-        Log.d(TAG, "getCollapsedText: lastIndex -> $lastIndexOfText")
-        Log.d(TAG, "getCollapsedText: text -> $text")
-        if (text != null) {
-            Log.d(TAG, "getCollapsedText: text size -> ${text.length}")
-        }
+
+    private fun getCollapsedText(text: CharSequence?): CharSequence {
+
         val trimEndIndex: Int = if (lastIndexOfText == DEFAULT_INDEX) {
             text?.length ?: 0
         } else {
             lastIndexOfText - (ELLIPSIZE.length +
                     (collapsedText?.length ?: 0) + 1)
         }
-
-
-        Log.d(TAG, "getCollapsedText: trimIndex -> $trimEndIndex")
 
         val stringBuilder = SpannableStringBuilder(text, 0, trimEndIndex)
             .append(ELLIPSIZE)
@@ -87,15 +85,45 @@ class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
             Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
         )
 
-        Log.d(TAG, "getCollapsedText: string builder -> $stringBuilder")
-
-        return stringBuilder
+        return addClickableSpan(stringBuilder, collapsedText ?: "")
     }
 
-//    override fun setText(text: CharSequence?, type: BufferType?) {
-//        this.textString = text
-//        bufferType = type
-//    }
+    private fun getExpandedText(text: CharSequence?): CharSequence? {
+        if (!readMore) {
+            val stringBuilder = SpannableStringBuilder(text, 0, text?.length ?: 0)
+                .append(ELLIPSIZE)
+                .append(expandedText ?: "")
+
+            val fcs = ForegroundColorSpan(Color.parseColor("#4287f5"))
+
+            stringBuilder.setSpan(
+                fcs,
+                stringBuilder.length - (expandedText?.length ?: 0),
+                stringBuilder.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            return addClickableSpan(stringBuilder, expandedText ?: "")
+        }
+
+        return text
+    }
+
+    private fun addClickableSpan(s: SpannableStringBuilder, trimText: CharSequence): CharSequence {
+        s.setSpan(
+            viewMoreSpan,
+            s.length - trimText.length,
+            s.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        return s
+    }
+
+    inner class ShowMoreClickableSpan : ClickableSpan() {
+        override fun onClick(widget: View) {
+            readMore = !readMore
+            setText()
+        }
+    }
 
     private fun initializingViewTreeObserver() {
         viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
@@ -109,14 +137,19 @@ class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
     }
 
     private fun calculateLastIndex() {
-        Log.d(TAG, "calculateLastIndex: lineCount -> $lineCount and maxLines -> $maxLinesVisible")
         try {
-            lastIndexOfText = if (maxLinesVisible == 0) {
-                layout.getLineEnd(0)
-            } else if (maxLinesVisible > 0 && maxLinesVisible > lineCount) {
-                layout.getLineEnd(maxLinesVisible - 1)
-            } else {
-                DEFAULT_INDEX
+            lastIndexOfText = when (maxLinesVisible) {
+                0 -> {
+                    layout.getLineEnd(0)
+                }
+                in 1 until lineCount -> {
+                    showFullExpandedText = false
+                    layout.getLineEnd(maxLinesVisible - 1)
+                }
+                else -> {
+                    showFullExpandedText = true
+                    DEFAULT_INDEX
+                }
             }
         } catch (e: Exception) {
             throw e
@@ -125,24 +158,39 @@ class ShowMoreTextView(context: Context, attrs: AttributeSet?) :
 
     init {
         val typedArray =
-            context.theme.obtainStyledAttributes(attrs, R.styleable.ShowMoreTextView, 0, 0)
-        collapsedText = resources.getString(
-            typedArray.getResourceId(
-                R.styleable.ShowMoreTextView_collapsedText,
-                R.string.show_more
+            context.obtainStyledAttributes(attrs, R.styleable.ShowMoreTextView)
+
+        collapsedText =
+            typedArray.getString(
+                R.styleable.ShowMoreTextView_collapsedText
             )
-        )
-        expandedText = resources.getString(
-            typedArray.getResourceId(
-                R.styleable.ShowMoreTextView_expandedText,
-                R.string.show_less
+
+        expandedText =
+            typedArray.getString(
+                R.styleable.ShowMoreTextView_expandedText
             )
-        )
-        Log.d(TAG, "get Collapsed text : $collapsedText")
-        maxLinesVisible = typedArray.getResourceId(
+
+        /*
+            Set default string if collapsed text is null i.e. "Show more"
+         */
+        if (collapsedText == null) {
+            collapsedText = resources.getString(R.string.show_more)
+        }
+
+        /*
+            Set default string if expanded text is null i.e. "Show less"
+         */
+        if (expandedText == null) {
+            expandedText = resources.getString(R.string.show_less)
+        }
+
+        maxLinesVisible = typedArray.getInt(
             R.styleable.ShowMoreTextView_maxLinesVisible,
             DEFAULT_MAX_LINE
         )
+
+        textString = text
+        
         initializingViewTreeObserver()
         typedArray.recycle()
     }
